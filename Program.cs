5ee364dbd08b90650863981bc8dc18d18c4d0e30
Marek <marek@adameczek.pl>
2026-01-30 15:21:27 +0100

    Check is OS LInux

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ using choinka.Triggers.SolarTime.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System.Runtime.InteropServices;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", Serilog.Events.LogEventLevel.Information)
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
 
 try
 {
+    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        Log.Error("This application is intended to run on Linux systems " +
+            "with systemd and GPIO support like RaspberryPi");
+        return;
+    }
+
     var hostBuilder = Host.CreateDefaultBuilder();
     hostBuilder.UseSystemd();
     hostBuilder.ConfigureServices((ctx, services) =>
